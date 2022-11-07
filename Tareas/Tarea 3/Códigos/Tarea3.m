b0d56ecb808ae60f00382d7fd86c79bc36d5aaa8
Alexis Contreras R <ale.aicr@gmail.com>
2022-11-06 23:08:12 -0300

    Init Tarea 3

--- Tareas/Tarea 3/Códigos/Tarea3.m
+++ Tareas/Tarea 3/Códigos/Tarea3.m
@@ -7,10 +7,78 @@ close all
 clc
 
 %% Inputs
+g = 9.81; % m/s2
+cant_estr = 2;                                                              % Cantidad de estructutras
+cant_variantes = 4;                                                         % Cantidad de variantes por cada estructura
 
+ResultsFile = ["est_1_A";"est_1_B";"est_1_C";"est_1_D";"est_2_A";"est_2_B";"est_2_C";"est_2_D"];
+ResultsDir = "IIDAP_T3";
+n_ests = length(ResultsFile);
+
+%% Load Data
+Data = struct();
+for i = 1:n_ests
+    [Data(i).EDP,Data(i).IM,Data(i).IMc,Data(i).Backbone] = getIdaCurves_v2(convertStringsToChars(ResultsDir), convertStringsToChars(ResultsFile(i)));
+end
+
+% Tenemos todos los datos para cada una de las 2 estructuras 4 variantes enumeradas del 1 al 8 
+% (1,2,3,4 son est1 A,B,C,D y 5,6,7,8 son est2 A,B,C,D respectivamente)
+% De haber una tercera estructura entonces 9,10,11,12 de est3 A,B,C,D
+% Ej: Data(1).EDP = matriz EDP que retorna getIdaCurves_v2 para estructura 1 A
 
 %% P1
+% Graficar 
+% Para graficar se necesita dy dp dpc du; Fy Fmax Fres
 
+for i = 1:cant_estr
+    figure
+    hold on
+    for j = 1:cant_variantes
+        dy = Data((i-1)*cant_variantes+j).Backbone.Uy_pos0;
+        dp = Data((i-1)*cant_variantes+j).Backbone.Up_pos0;
+        dpc = Data((i-1)*cant_variantes+j).Backbone.Upc_pos0;
+        du = Data((i-1)*cant_variantes+j).Backbone.Uu_pos0;
+        dres = Data((i-1)*cant_variantes+j).Backbone.Ures_pos0;
+        fy = Data((i-1)*cant_variantes+j).Backbone.Fy_pos0;
+        fmax = Data((i-1)*cant_variantes+j).Backbone.FmaxFy_pos0*fy;
+        fres = Data((i-1)*cant_variantes+j).Backbone.FresFy_pos0*fy; 
+        X = [0; dy; dp; dres; du; du];
+        Y = [0; fy; fmax; fres; fres; 0];
+        plot(X,Y)
+    end
+    hold off
+    xlabel('\delta [m]')
+    ylabel('F [kN]')
+    title('Backbone IMK Bilinear Model')
+    legend('A','B','C','D')
+    grid on
+end
 
+%% P2
+% Graficar curvas IDA
+for i = 1:n_ests
+    figure
+    plot(Data(i).EDP,Data(i).IM./g,'.-','color','#606060')
+    xlabel('EDP: desplazamiento latereal [m]')
+    ylabel('IM: Sa(T_1) [g]')
+    title('Curvas IDA')
+    grid on
+end
 
+%% P3
+% Graficar las curvas de fragilidad de colapso para cada estructura
+% (modelo)
 
+% Por separado
+for i = 1:n_estr
+    figure
+    plot()
+end
+
+% Todas juntas
+for i = 1:cant_estr
+    figure
+    for j = 1:cant_variantes
+
+    end
+end
--- Tareas/Tarea 3/Códigos/getIdaCurves_v2.m
+++ Tareas/Tarea 3/Códigos/getIdaCurves_v2.m
@@ -1,4 +1,5 @@
-function [EDP,IM,IMc] = getIdaCurves_v2(ResultsFile)
+function [EDP,IM,IMc,Backbone_Data] = getIdaCurves_v2(ResultsDir, ResultsFile)
+% Modificada por: Alexis Contreras en Tarea 3 para obtener backbone 
 
 % Version    : 2.0
 % Creado por : Cristian Cruz (CC) y Pablo Heresi (PH)
@@ -45,37 +46,43 @@ function [EDP,IM,IMc] = getIdaCurves_v2(ResultsFile)
 %
 % IMc(1,nRegistros):        Vector con el IM de colapso de cada registro.
 %                           Unidades: [g].
+% Backbone_Data:                 Vector columna con los datos para graficar la
+%                           Backbone (aprovechando que se abren los datos
+%                           dentro de esta función).
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Cargar resultados
-load(ResultsFile)
+A = load([ResultsDir '\' ResultsFile]);
 
 % variable nGM indica el numero de registros
 % variable IDA contiene resultados del analisis
 
 %% Calcular numero de franjas máximo de todos los registros
 nStripes = 0;
-for i = 1:nGM
-    nStripes = max(nStripes, length(IDA.(['Sa' num2str(i)]))-1);
+for i = 1:A.nGM
+    nStripes = max(nStripes, length(A.IDA.(['Sa' num2str(i)]))-1);
 end
 
 %% Cargar EDP y IM en matrices:
 
-g    = 9.81;                   % Ac. gravedad
+g = 9.81;                   % Ac. gravedad
 
 % Inicializar variables:
-EDP = nan(nStripes,nGM);
-IM  = nan(nStripes,nGM);
-IMc = zeros(1,nGM);
+EDP = nan(nStripes,A.nGM);
+IM  = nan(nStripes,A.nGM);
+IMc = zeros(1,A.nGM);
 
 % Cargar datos:
-for i = 1:nGM
-    nStripes_i = length(IDA.(['Sa' num2str(i)]))-1;
-    EDP(1:nStripes_i,i) = IDA.(['U' num2str(i)])(1:nStripes_i);
-    IM(1:nStripes_i,i) =  IDA.(['Sa' num2str(i)])(1:nStripes_i)/g;
-    IMc(i) = IDA.(['Sa' num2str(i)])(end)/g;
+for i = 1:A.nGM
+    nStripes_i = length(A.IDA.(['Sa' num2str(i)]))-1;
+    EDP(1:nStripes_i,i) = A.IDA.(['U' num2str(i)])(1:nStripes_i);
+    IM(1:nStripes_i,i) =  A.IDA.(['Sa' num2str(i)])(1:nStripes_i)/g;
+    IMc(i) = A.IDA.(['Sa' num2str(i)])(end)/g;
 end
+
+% Gurdar Backbone
+Backbone_Data = A.Backbone;
 
 
